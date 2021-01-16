c32da710a33e60f961d03ead2f37a128e18b6e28
Victor Piamias <victorpiamiaspro@outlook.fr>
2021-01-17 00:07:10 +0100

    Firstinit

---
+++ fibo.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int fibo(int n);
+
+int main()
+{
+	int n;
+
+	n = 0;
+	printf("Enter the place number in Fibonnaci sequence : \n");
+	scanf("%d",&n);
+	n = fibo(n);
+	printf("\n%d\n",n);
+	return 0;
+}
+
+int fibo(int n)
+{
+	int sequence[3] = {0,1,1};
+	int i;
+		
+	i = 2;
+	if (n > 1)
+	{		
+		while (n > 1)
+		{
+			sequence[i] = sequence[i-1]	+ sequence [i-2];
+			sequence[i-2] = sequence[i-1];	
+			sequence[i-1] = sequence[i];	
+			n --;
+		}	
+	}	
+	else
+	{
+		if (n == 0)
+		{
+			i = 0;
+		}		
+	}
+	n = sequence[i];
+	return n;	
+}
+
